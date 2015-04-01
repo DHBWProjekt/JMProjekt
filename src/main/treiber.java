34963bcee97b0cd850e4a11516d287d6bb0374f0
Kai <SCCplayer@gmx.de>
2015-04-01 13:18:45 +0200

    Aktueller Stand wird hochgeladen

--- src/main/treiber.java
+++ src/main/treiber.java
@@ -5,6 +5,7 @@ public class treiber {
 	public static void main(String[] args) {
 		// Kommentar nur zum testen
 		// TODO Auto-generated method stub
+		// Das öffnen der Classfile hat eine Änderung im ByteCode verursacht
 
 	}
 
