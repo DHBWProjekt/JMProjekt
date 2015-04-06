0a5d3885b68ed46710386a52aa0a8b1e724e38a8
Kai <SCCplayer@gmx.de>
2015-04-06 11:15:51 +0200

    JMPlayer wurde bearbeitet

--- src/gui/JMPlayer.java
+++ src/gui/JMPlayer.java
@@ -2,47 +2,51 @@ package gui;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.GridLayout;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
- 
-public class JMPlayer{
-	public static void main( String[] args ){
+import javax.swing.JTextArea;
+
+public class JMPlayer {
+	public static void main(String[] args) {
 		// Die Daten für das Table
-		String[][] data = new String[][]{
-				{"Reich und Sexy", "Die Toten Hosen", "Rock", "3:25"},
-				{"Blue", "Eifel 65", "Pop", "3:22"},
-				{"Türlich Türlich", "Das Bo", "Hip-Hop", "3:45"},
-				{"Flat Beat","Mr. Oreo","Dance","3:22"}
-		};
- 
+		String[][] data = new String[][] {
+				{ "Reich und Sexy", "Die Toten Hosen", "Rock", "3:25" },
+				{ "Blue", "Eifel 65", "Pop", "3:22" },
+				{ "Türlich Türlich", "Das Bo", "Hip-Hop", "3:45" },
+				{ "Flat Beat", "Mr. Oreo", "Dance", "3:22" },
+				{ "Westerland", "Die Ärzte", "Alternativ", "3:22" } };
+
 		// Die Column-Titles
-		String[] title = new String[]{
-				"Titel", "Interpret", "Genre", "Dauer"
-		};
- 
+		String[] title = new String[] { "Titel", "Interpret", "Genre", "Dauer" };
+
 		// Das JTable initialisieren
-//		JTable table = new JTable( data, title );
+		// JTable table = new JTable( data, title );
 		JPanel btnPanel = new JPanel(new FlowLayout());
+		JPanel northPanel = new JPanel(new GridLayout(2, 1));
+		northPanel.add(new JTextArea());
 		btnPanel.add(new JButton("Play"));
 		btnPanel.add(new JButton("Pause"));
 		btnPanel.add(new JButton("Stopp"));
 		btnPanel.add(new JButton("Next"));
 		btnPanel.add(new JButton("Back"));
-//		tblPanel.add(new JScrollPane(table));
-		
+		// tblPanel.add(new JScrollPane(table));
+
 		// Das JTable initialisieren
-		JTable table = new JTable( data, title );
- 
-		JFrame frame = new JFrame( "JMPlayer" );
+		JTable table = new JTable(data, title);
+
+		JFrame frame = new JFrame("JMPlayer");
 		frame.setLayout(new BorderLayout());
-		frame.add(btnPanel,BorderLayout.NORTH);
-		frame.getContentPane().add(new JScrollPane(table),BorderLayout.CENTER);
-		frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		northPanel.add(btnPanel);
+		frame.add(northPanel, BorderLayout.NORTH);
+		frame.getContentPane().add(new JScrollPane(table), BorderLayout.CENTER);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.pack();
-		frame.setVisible( true );
+		frame.setVisible(true);
+
 	}
 }
